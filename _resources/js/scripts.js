fe62a26bd45cf4a24e164757d2738dc09a361bc0
Kelley David Cole <mdigerat@gmail.com>
2022-04-08 10:08:02 -0400

    Add files via upload
    added /_resources

---
+++ _resources/js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+$('body').removeClass('jsOff').addClass('jsOn');
+
+$('#tabs a').on('click', function(e){
+	e.preventDefault();
+	$('#tabs a.current').removeClass('current');
+	$('.tab-section:visible').hide();
+	$(this.hash).show();
+	$(this).addClass('current');
+}).filter(':first').click();
+  $('a[rel="gaSimCenter"]').colorbox({
+    width: '75%',
+    height: '70%',
+    slideshow: true,
+    slideshowSpeed: 2000,
+    slideshowAuto: false,
+    slideshowStart: 'Start Slide Show',
+    slideshowStop: 'Stop Slide Show'
+  });
+    $('a[rel="gaSimCenterVideos"]').colorbox({
+    iframe: true,
+    innerWidth: 640,
+    innerHeight: 390,
+    current: 'Video {current} of {total}'
+  });
+});
